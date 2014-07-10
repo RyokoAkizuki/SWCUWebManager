649be305e38832aa655f01a791f5d1b47f403433
Yukino Hayakawa <freshrat912@gmail.com>
2014-07-10 14:10:47 +0800

    use rand to avoid cache

--- ajax_map_list.php
+++ ajax_map_list.php
@@ -11,15 +11,15 @@ foreach($list->children() as $child)
     echo('<tr><th class="griditemname">' . $child["name"] . '</th>');
     echo('<th>' . (
         ($child["autoload"] == "1") ?
-            ('<a href="rpc_map_set_autoload.php?autoload=false&name=' . $child["name"] . '"">禁用</a>') :
-            ('<a href="rpc_map_set_autoload.php?autoload=true&name=' . $child["name"] . '"">启用</a>'))
+            ('<a href="rpc_map_set_autoload.php?rand=' . rand() . '&autoload=false&name=' . $child["name"] . '"">禁用</a>') :
+            ('<a href="rpc_map_set_autoload.php?rand=' . rand() . '&autoload=true&name=' . $child["name"] . '"">启用</a>'))
         . '</th>');
     echo('<th>' . (
         ($child["loaded"] == "1") ?
-            ('<a href="rpc_map_unload.php?name=' . $child["name"] . '"">卸载</a>') :
-            ('<a href="rpc_map_load.php?name=' . $child["name"] . '"">加载</a>'))
+            ('<a href="rpc_map_unload.php?rand=' . rand() . '&name=' . $child["name"] . '"">卸载</a>') :
+            ('<a href="rpc_map_load.php?rand=' . rand() . '&name=' . $child["name"] . '"">加载</a>'))
         . '</th>');
-    echo('<th><a href="rpc_map_dispose.php?name=' . $child["name"] . '"">删除</a></th></tr>');
+    echo('<th><a href="rpc_map_dispose.php?rand=' . rand() . '&name='  .$child["name"] . '"">删除</a></th></tr>');
 }
 
 ?>
