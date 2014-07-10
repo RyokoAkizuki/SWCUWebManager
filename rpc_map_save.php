8061a5911b82126851913efb6deae100837c2a67
Yukino Hayakawa <freshrat912@gmail.com>
2014-07-10 12:18:55 +0800

    ajax map saving

--- rpc_map_save.php
+++ rpc_map_save.php
@@ -2,12 +2,24 @@
 
 require("rpc.php");
 
-print("mapSave: " . xmlrpc_decode(mapSave($_POST["mapname"], $_POST["mapcode"], isset($_POST["autoload"]))) . "<br/>");
+$saved = (bool)xmlrpc_decode(mapSave($_POST["mapname"], $_POST["mapcode"], isset($_POST["autoload"])));
+
+if(!$saved)
+{
+	print("地图未能成功保存. 详情请查看服务器记录.<br/>");
+	exit();
+}
+
+print("地图保存成功<br/>");
+
+$loaded = false;
 
 if(isset($_POST["loadnow"]))
 {
-	print("mapLoad: " . xmlrpc_decode(mapLoad($_POST["mapname"])));
+	$loaded = (xmlrpc_decode(mapLoad($_POST["mapname"])) == "1");
 }
+
+print("地图载入" . ($loaded ? "成功" : "失败") . "<br/>");
 
 ?>
 
